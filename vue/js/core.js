2ecbf43ae9f9a548b729fda3dfe7685a646aeed2
Tianyi He <tianyi.he@morningstar.com>
2017-08-15 17:07:16 -0500

    update vue progress

--- vue/js/core.js
+++ vue/js/core.js
@@ -1,6 +1,28 @@
-var timer = new Vue({
-    el: 'header',
+var example1 = new Vue({
+    el: '#ex-1',
     data: {
       timer: '' + new Date()
     }
+});
+
+var example2 = new Vue({
+  el: '#ex-2',
+  data: {
+    message: 'This page is loaded at ' + new Date()
+  }
+});
+
+var example3 = new Vue({
+  el: '#ex-3',
+  data: {
+    show: true
+  }
+});
+
+setInterval(function() {
+  example1.timer = '' + new Date();
+}, 1000);
+
+document.getElementById('show-button').addEventListener('click', function() {
+  example3.show = !example3.show;
 });
